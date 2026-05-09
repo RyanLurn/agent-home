97e77a619917c9fecdfdde2c229240da6374e8d5
Ryan Luong <lumintriet@gmail.com>
2026-05-09 16:22:46 +0700

    chore(db): generate migration files.

---
+++ packages/db/migrations/20260509090952_skinny_norman_osborn/migration.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `context_entries` (
+	`id` text PRIMARY KEY,
+	`author` text NOT NULL,
+	`content` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`deleted_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `notifications` (
+	`id` text PRIMARY KEY,
+	`source` text NOT NULL,
+	`content` text NOT NULL,
+	`status` text DEFAULT 'pending' NOT NULL,
+	`created_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`deleted_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `messages` (
+	`id` text PRIMARY KEY,
+	`sender` text NOT NULL,
+	`content` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch() * 1000) NOT NULL,
+	`deleted_at` integer
+);
