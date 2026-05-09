f748403790764dab38b2e1941f64d18d8d051588
Ryan Luong <lumintriet@gmail.com>
2026-05-09 16:33:31 +0700

    feat(db/types): define InsertedMessage type.

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,3 +1,4 @@
 import type { messageTable } from "@/schema/tables/message";
 
 export type SelectedMessage = typeof messageTable.$inferSelect;
+export type InsertedMessage = typeof messageTable.$inferInsert;
