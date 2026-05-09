b5e5a9a3a301ab0ad5e39ba757325dc3db535dff
Ryan Luong <lumintriet@gmail.com>
2026-05-09 18:48:44 +0700

    feat(db/queries): define conceptual Result type for insertOneMessage function.

--- packages/db/src/queries/messages/insert-one.ts
+++ packages/db/src/queries/messages/insert-one.ts
@@ -1,13 +1,27 @@
-import type { InsertedMessage } from "@/types";
+import type { Result } from "@repo/core/types/result";
+
+import type { InsertedMessage, SelectedMessage } from "@/types";
 
 import { messageTable } from "@/schema/tables/message";
 import { db } from "@/index";
 
-export async function insertOneMessage(message: InsertedMessage) {
+export async function insertOneMessage(
+  message: InsertedMessage
+): Promise<Result<SelectedMessage, Error>> {
   const [returnedMessage] = await db
     .insert(messageTable)
     .values(message)
     .returning();
 
-  return returnedMessage ?? null;
+  if (returnedMessage) {
+    return {
+      success: true,
+      data: returnedMessage,
+    };
+  }
+
+  return {
+    success: false,
+    error: new Error("Edge case"),
+  };
 }
