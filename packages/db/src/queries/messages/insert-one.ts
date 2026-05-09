7cfec2f4113fffa76e6f9625b7d20e9a57cfb7f8
Ryan Luong <lumintriet@gmail.com>
2026-05-09 19:17:58 +0700

    feat(db/queries): enhance error handling in insertOneMessage function.

--- packages/db/src/queries/messages/insert-one.ts
+++ packages/db/src/queries/messages/insert-one.ts
@@ -1,5 +1,11 @@
 import type { Result } from "@repo/core/types/result";
 
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@repo/core/error/create-fallback";
+import { SQLiteError } from "bun:sqlite";
+
 import type { InsertedMessage, SelectedMessage } from "@/types";
 
 import { messageTable } from "@/schema/tables/message";
@@ -7,21 +13,42 @@ import { db } from "@/index";
 
 export async function insertOneMessage(
   message: InsertedMessage
-): Promise<Result<SelectedMessage, Error>> {
-  const [returnedMessage] = await db
-    .insert(messageTable)
-    .values(message)
-    .returning();
+): Promise<Result<SelectedMessage, FallBackError | SQLiteError>> {
+  const startContext = {
+    inputs: message,
+    processName: "insertOneMessage",
+  };
 
-  if (returnedMessage) {
+  try {
+    const [returnedMessage] = await db
+      .insert(messageTable)
+      .values(message)
+      .returning();
+
     return {
       success: true,
-      data: returnedMessage,
+      /**
+       * returnedMessage should never be undefined.
+       * Type assertion exists to make TypeScript happy.
+       */
+      data: returnedMessage as SelectedMessage,
+    };
+  } catch (error) {
+    if (error instanceof SQLiteError) {
+      return {
+        success: false,
+        error,
+      };
+    }
+
+    return {
+      success: false,
+      error: createFallbackError({
+        message:
+          "Failed to insert message into the database due to an unexpected error",
+        context: startContext,
+        cause: error,
+      }),
     };
   }
-
-  return {
-    success: false,
-    error: new Error("Edge case"),
-  };
 }
