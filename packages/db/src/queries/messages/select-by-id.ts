81cb224e7ba3246724a5c065509236e7260c0851
Ryan Luong <lumintriet@gmail.com>
2026-05-10 08:20:23 +0700

    feat(db/queries): selectMessageById now returns MessageDTO instead.

--- packages/db/src/queries/messages/select-by-id.ts
+++ packages/db/src/queries/messages/select-by-id.ts
@@ -3,27 +3,26 @@ import type { Result } from "@repo/core/types/result";
 import { NotFoundError } from "@repo/core/error/classes/not-found";
 import { isNull, and, eq } from "drizzle-orm";
 
-import type { Message } from "@/types";
+import type { SelectedMessage } from "@/types";
 
+import { createMessageDTO, type MessageDTO } from "@/dto/message";
 import { messageTable } from "@/schema/tables/message";
 import { db } from "@/index";
 
 export async function selectMessageById({
   id,
 }: {
-  id: Message["id"];
-}): Promise<Result<Message, NotFoundError<"message">>> {
+  id: SelectedMessage["id"];
+}): Promise<Result<MessageDTO, NotFoundError<"message">>> {
   const [selectedMessage] = await db
     .select()
     .from(messageTable)
     .where(and(eq(messageTable.id, id), isNull(messageTable.deletedAt)));
 
   if (selectedMessage) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { deletedAt, ...message } = selectedMessage;
     return {
       success: true,
-      data: message,
+      data: createMessageDTO(selectedMessage),
     };
   }
 
