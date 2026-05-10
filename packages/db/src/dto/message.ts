14a85212fe7eea17c9c48464bb309f800e5ad0c5
Ryan Luong <lumintriet@gmail.com>
2026-05-10 09:00:07 +0700

    feat(db/dto): create MessageDTOSchema.

--- packages/db/src/dto/message.ts
+++ packages/db/src/dto/message.ts
@@ -1,4 +1,8 @@
+import { z } from "zod";
+
 import type { SelectedMessage } from "@/types";
+
+import { MESSAGE_SENDERS } from "@/schema/constants";
 
 export function createMessageDTO(selectedMessage: SelectedMessage) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -11,4 +15,12 @@ export function createMessageDTO(selectedMessage: SelectedMessage) {
   };
 }
 
-export type MessageDTO = ReturnType<typeof createMessageDTO>;
+export const MessageDTOSchema = z.strictObject({
+  id: z.uuidv4(),
+  sender: z.enum(MESSAGE_SENDERS),
+  content: z.string(),
+  createdAt: z.iso.datetime(),
+  updatedAt: z.iso.datetime(),
+});
+
+export type MessageDTO = z.infer<typeof MessageDTOSchema>;
