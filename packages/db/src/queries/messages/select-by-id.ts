d55a5e32b98de34288074f70b0c69b4976eacdd2
Ryan Luong <lumintriet@gmail.com>
2026-05-10 07:39:04 +0700

    feat(db/queries): define Result type for selectMessageById function.

--- packages/db/src/queries/messages/select-by-id.ts
+++ packages/db/src/queries/messages/select-by-id.ts
@@ -1,3 +1,6 @@
+import type { Result } from "@repo/core/types/result";
+
+import { NotFoundError } from "@repo/core/error/classes/not-found";
 import { isNull, and, eq } from "drizzle-orm";
 
 import type { Message } from "@/types";
@@ -5,11 +8,27 @@ import type { Message } from "@/types";
 import { messageTable } from "@/schema/tables/message";
 import { db } from "@/index";
 
-export async function selectMessageById({ id }: { id: Message["id"] }) {
+export async function selectMessageById({
+  id,
+}: {
+  id: Message["id"];
+}): Promise<Result<Message, NotFoundError<"message">>> {
   const [selectedMessage] = await db
     .select()
     .from(messageTable)
     .where(and(eq(messageTable.id, id), isNull(messageTable.deletedAt)));
 
-  return selectedMessage;
+  if (selectedMessage) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { deletedAt, ...message } = selectedMessage;
+    return {
+      success: true,
+      data: message,
+    };
+  }
+
+  return {
+    success: false,
+    error: new NotFoundError<"message">({ context: { resource: "message" } }),
+  };
 }
