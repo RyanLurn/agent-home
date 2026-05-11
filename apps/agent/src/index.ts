786d2a9421039ce9ffabb8077e584d86d3412901
Ryan Luong <lumintriet@gmail.com>
2026-05-11 08:55:10 +0700

    feat(agent/auto): update the auto-mode loop with 2 steps: getting agent's state and calling the LLM API.

--- apps/agent/src/index.ts
+++ apps/agent/src/index.ts
@@ -1,8 +1,26 @@
+import { generateText } from "ai";
+
+import { mapContextEntriesToAISDKModelMessages } from "@/utils/data-mapping";
+import { googleGenAIProvider } from "@/lib/llm-providers/google-genai";
+import { getState } from "@/get-state";
+
 const mode: "standby" | "auto" = "auto";
 let turn = 0;
 
 while (mode === "auto") {
   turn++;
   console.log(`[AGENT] Turn ${turn}`);
-  await Bun.sleep(500);
+
+  // Get agent's state
+  const state = await getState();
+  const messages = mapContextEntriesToAISDKModelMessages(state.contextEntries);
+
+  // Call LLM API
+  const { text } = await generateText({
+    model: googleGenAIProvider("gemma-4-31b-it"),
+    messages,
+  });
+  console.log(`[AGENT LLM] Text: ${text}`);
+
+  // TODO: Execute script in sandbox
 }
