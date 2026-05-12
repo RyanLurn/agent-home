30aec6827cda1d45dbb7dea6deb07ffcea71368a
Ryan Luong <lumintriet@gmail.com>
2026-05-12 09:03:01 +0700

    feat(user-server): define upgrade to websocket protocol endpoint.

--- apps/user-server/src/index.ts
+++ apps/user-server/src/index.ts
@@ -1,3 +1,21 @@
+import { upgradeWebSocket } from "hono/bun";
 import { Hono } from "hono";
 
-export const app = new Hono().get("/", (c) => c.text("Hello Bun!"));
+export const app = new Hono()
+  .get("/", (c) => c.text("Hello Bun!"))
+  .get(
+    "/ws",
+    upgradeWebSocket((c) => {
+      return {
+        onOpen(evt, ws) {
+          console.log("A wild client appeared!");
+        },
+        onMessage(evt, ws) {
+          console.log("The wild client used Send Message!");
+        },
+        onClose(evt, ws) {
+          console.log("The wild client ran away!");
+        },
+      };
+    })
+  );
