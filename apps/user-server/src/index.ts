c2a6641e1f47355fb5f520d4209483af09c87ab1
Ryan Luong <lumintriet@gmail.com>
2026-05-12 09:40:53 +0700

    feat(user-server/ws): send server's acknowledgement of client connection.

--- apps/user-server/src/index.ts
+++ apps/user-server/src/index.ts
@@ -3,16 +3,24 @@ import type { WSContext } from "hono/ws";
 import { upgradeWebSocket } from "hono/bun";
 import { Hono } from "hono";
 
+import type { ClientConnectedEnvelope } from "@/envelope";
+
 const clients: WSContext[] = [];
 
 export const app = new Hono()
   .get("/", (c) => c.text("Hello Bun!"))
   .get(
     "/ws",
-    upgradeWebSocket((c) => {
+    upgradeWebSocket(() => {
       return {
         onOpen(_evt, ws) {
           clients.push(ws);
+
+          const envelope: ClientConnectedEnvelope = {
+            code: "CLIENT_CONNECTED_EVENT",
+            sender: "server",
+          };
+          ws.send(JSON.stringify(envelope));
         },
         onMessage(evt, ws) {
           console.log("The wild client used Send Message!");
