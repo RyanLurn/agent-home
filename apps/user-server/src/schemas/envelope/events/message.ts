457d7514cafaaf0fb4a64fdbe1795bd34ad23d41
Ryan Luong <lumintriet@gmail.com>
2026-05-13 08:58:44 +0700

    feat(user-server/chat): make NewMessageEventSchema stricter.

--- apps/user-server/src/schemas/envelope/events/message.ts
+++ apps/user-server/src/schemas/envelope/events/message.ts
@@ -6,8 +6,11 @@ import { EnvelopeEmitterSchema } from "@/schemas/envelope/emitter";
 export const NewMessageEventSchema = z.object({
   id: z.uuidv4(),
   type: z.literal("message.new"),
-  emitter: EnvelopeEmitterSchema,
-  payload: MessageDTOSchema.pick({ sender: true, content: true }),
+  emitter: EnvelopeEmitterSchema.extract(["client"]),
+  payload: z.object({
+    sender: MessageDTOSchema.shape.sender.extract(["user"]),
+    content: MessageDTOSchema.shape.content,
+  }),
 });
 export type NewMessageEvent = z.infer<typeof NewMessageEventSchema>;
 
