6b9c6ec9de2212c5e29dbc6959e8a921f7fe0b37
Ryan Luong <lumintriet@gmail.com>
2026-05-13 07:36:58 +0700

    feat(user-server): remove unused files.

--- apps/user-server/src/schemas/ws-client-id.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-import type { WSClientId } from "@/types";
-
-export const WSClientIdSchema = z.custom<WSClientId>(
-  (value) => typeof value === "string" && value.length > 0
-);
--- apps/user-server/src/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Branded } from "@repo/core/types/branded";
-import type { ServerWebSocket } from "bun";
-import type { WSContext } from "hono/ws";
-
-export type WSClientId = Branded<string, "WSClientId">;
-export type WSClient = {
-  id: WSClientId;
-  ws: WSContext<ServerWebSocket>;
-};
