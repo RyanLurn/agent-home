f781d551cd3953fa2da32b8c78cadf92ca35b0d5
Ryan Luong <lumintriet@gmail.com>
2026-05-13 08:48:43 +0700

    feat(db/queries): simplify insertOneMessage function for this stage of development.

--- packages/db/src/queries/messages/insert-one.ts
+++ packages/db/src/queries/messages/insert-one.ts
@@ -1,15 +1,7 @@
-import type { Result } from "@repo/core/types/result";
-
-import {
-  createFallbackError,
-  type FallBackError,
-} from "@repo/core/error/create-fallback";
-import { SQLiteError } from "bun:sqlite";
-
 import type { InsertedMessage, SelectedMessage } from "@/types";
 
-import { createMessageDTO, type MessageDTO } from "@/dto/message";
 import { messageTable } from "@/schema/tables/message";
+import { createMessageDTO } from "@/dto/message";
 import { db } from "@/index";
 
 export async function insertOneMessage({
@@ -18,38 +10,12 @@ export async function insertOneMessage({
 }: {
   sender: InsertedMessage["sender"];
   content: InsertedMessage["content"];
-}): Promise<Result<MessageDTO, FallBackError | SQLiteError>> {
-  const startContext = {
-    inputs: { sender, content },
-    processName: "insertOneMessage",
-  };
+}) {
+  const [returnedMessage] = await db
+    .insert(messageTable)
+    .values({ sender, content })
+    .returning();
 
-  try {
-    const [returnedMessage] = await db
-      .insert(messageTable)
-      .values({ sender, content })
-      .returning();
-
-    return {
-      success: true,
-      // returnedMessage should never be undefined. Type assertion exists to make TypeScript happy.
-      data: createMessageDTO(returnedMessage as SelectedMessage),
-    };
-  } catch (error) {
-    if (error instanceof SQLiteError) {
-      return {
-        success: false,
-        error,
-      };
-    }
-
-    return {
-      success: false,
-      error: createFallbackError({
-        message: "Failed to insert message due to an unexpected error",
-        context: startContext,
-        cause: error,
-      }),
-    };
-  }
+  // returnedMessage should never be undefined. Type assertion exists to make TypeScript happy.
+  return createMessageDTO(returnedMessage as SelectedMessage);
 }
