6527b88187519bc789a43a0da67504d33d05b506
Ryan Luong <lumintriet@gmail.com>
2026-05-14 07:57:56 +0700

    feat(user-browser-client/chat): add getAllMessages server function to index route's loader.

--- apps/user-browser-client/src/routes/index.tsx
+++ apps/user-browser-client/src/routes/index.tsx
@@ -2,9 +2,17 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Button } from "@repo/ui/components/button";
 import { toast } from "@repo/ui/components/toaster";
 
-export const Route = createFileRoute("/")({ component: IndexPage });
+import { getAllMessages } from "@/features/chat/server-functions/get-all-messages";
+
+export const Route = createFileRoute("/")({
+  loader: () => getAllMessages(),
+  component: IndexPage,
+});
 
 function IndexPage() {
+  const messages = Route.useLoaderData();
+  console.log(messages);
+
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-y-2">
       <h1 className="text-2xl font-bold">Ultra Bun</h1>
