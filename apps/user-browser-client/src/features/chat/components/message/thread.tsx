4743189de9130e30faa602b26a8437242ef32343
Ryan Luong <lumintriet@gmail.com>
2026-05-14 08:05:06 +0700

    feat(user-browser-client/chat): define MessageThread component.

---
+++ apps/user-browser-client/src/features/chat/components/message/thread.tsx
@@ -0,0 +1,27 @@
+import type { MessageDTO } from "@repo/db/dto/message";
+import type { ComponentProps } from "react";
+
+import { cn } from "@repo/ui/lib/utils";
+
+import { MessageBubble } from "@/features/chat/components/message/bubble";
+
+interface MessageThreadProps extends ComponentProps<"div"> {
+  messages: MessageDTO[];
+}
+
+export function MessageThread({
+  messages,
+  className,
+  ...props
+}: MessageThreadProps) {
+  return (
+    <div
+      className={cn("flex size-full flex-col gap-y-6", className)}
+      {...props}
+    >
+      {messages.map((message) => (
+        <MessageBubble message={message} key={message.id} />
+      ))}
+    </div>
+  );
+}
