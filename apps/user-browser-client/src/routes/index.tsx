f013de1dc825187488794c5b3a2ac5112d8619d3
Ryan Luong <lumintriet@gmail.com>
2026-05-14 08:08:14 +0700

    feat(user-browser-client/chat): add MessageThread component to the IndexPage and pass the messages state to its prop.

--- apps/user-browser-client/src/routes/index.tsx
+++ apps/user-browser-client/src/routes/index.tsx
@@ -1,8 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@repo/ui/components/button";
-import { toast } from "@repo/ui/components/toaster";
 
 import { getAllMessages } from "@/features/chat/server-functions/get-all-messages";
+import { MessageThread } from "@/features/chat/components/message/thread";
 
 export const Route = createFileRoute("/")({
   loader: () => getAllMessages(),
@@ -11,12 +10,10 @@ export const Route = createFileRoute("/")({
 
 function IndexPage() {
   const messages = Route.useLoaderData();
-  console.log(messages);
 
   return (
-    <div className="flex h-dvh flex-col items-center justify-center gap-y-2">
-      <h1 className="text-2xl font-bold">Ultra Bun</h1>
-      <Button onClick={() => toast.success("Yippee!")}>Click me!</Button>
+    <div className="mx-auto flex size-full max-w-2xl flex-col items-center gap-y-6">
+      <MessageThread className="my-6 flex-1" messages={messages} />
     </div>
   );
 }
