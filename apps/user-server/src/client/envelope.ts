4ed9ecab5b015ae525a3f512ae79ef33602f0d3c
Ryan Luong <lumintriet@gmail.com>
2026-05-14 08:27:33 +0700

    feat(user-server): create an envelope barrel file in the client directory.

---
+++ apps/user-server/src/client/envelope.ts
@@ -0,0 +1,5 @@
+export * from "@/schemas/envelope/events/acknowledge";
+export * from "@/schemas/envelope/events/message";
+export * from "@/schemas/envelope/events/error";
+export * from "@/schemas/envelope/emitter";
+export * from "@/schemas/envelope/index";
