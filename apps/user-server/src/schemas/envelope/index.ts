99a9ed42ebe413c05bb4f880225f6a1e8524ba93
Ryan Luong <lumintriet@gmail.com>
2026-05-15 09:34:28 +0700

    feat(user-server/ws): add broadcast event to envelope schema.

--- apps/user-server/src/schemas/envelope/index.ts
+++ apps/user-server/src/schemas/envelope/index.ts
@@ -1,12 +1,16 @@
 import { z } from "zod";
 
+import {
+  NewMessageBroadcastEventSchema,
+  NewMessageEventSchema,
+} from "@/schemas/envelope/events/message";
 import { AcknowledgeEventSchema } from "@/schemas/envelope/events/acknowledge";
-import { NewMessageEventSchema } from "@/schemas/envelope/events/message";
 import { ErrorEventSchema } from "@/schemas/envelope/events/error";
 
 export const EnvelopeSchema = z.discriminatedUnion("type", [
   AcknowledgeEventSchema,
   NewMessageEventSchema,
+  NewMessageBroadcastEventSchema,
   ErrorEventSchema,
 ]);
 export type Envelope = z.infer<typeof EnvelopeSchema>;
