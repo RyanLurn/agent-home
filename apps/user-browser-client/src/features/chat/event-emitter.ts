180c79f243fc70c36c9c0a3b4b4551060af29e1e
Ryan Luong <lumintriet@gmail.com>
2026-05-16 09:21:07 +0700

    feat(user-browser-client/chat): create NewMessageEventSchema and infer its type.

--- apps/user-browser-client/src/features/chat/event-emitter.ts
+++ apps/user-browser-client/src/features/chat/event-emitter.ts
@@ -1,3 +1,12 @@
+import { MessageDTOSchema } from "@repo/db/dto/message";
 import EventEmitter from "node:events";
+import { z } from "zod";
 
 export const chatEventEmitter = new EventEmitter();
+
+export const NewMessageEventSchema = z.object({
+  event: z.literal("new-message"),
+  id: z.uuidv7(),
+  data: MessageDTOSchema,
+});
+export type NewMessageEvent = z.infer<typeof NewMessageEventSchema>;
