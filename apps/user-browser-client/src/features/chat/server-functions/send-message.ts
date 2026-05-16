2b37cc4d3506f6519c1474041a73b55c42475c7a
Ryan Luong <lumintriet@gmail.com>
2026-05-16 09:52:57 +0700

    feat(user-browser-client/chat): emit new message event in sendMessage server function.

--- apps/user-browser-client/src/features/chat/server-functions/send-message.ts
+++ apps/user-browser-client/src/features/chat/server-functions/send-message.ts
@@ -2,6 +2,8 @@ import { insertOneMessage } from "@repo/db/queries/messages/insert-one";
 import { MessageDTOSchema } from "@repo/db/dto/message";
 import { createServerFn } from "@tanstack/react-start";
 
+import { emitNewMessageEvent } from "@/features/chat/event-emitter";
+
 const SentMessageSchema = MessageDTOSchema.pick({ content: true });
 
 export const sendMessage = createServerFn({ method: "POST" })
@@ -11,5 +13,5 @@ export const sendMessage = createServerFn({ method: "POST" })
       sender: "user",
       content: data.content,
     });
-    console.log("Created message:", returnedMessage);
+    emitNewMessageEvent(returnedMessage);
   });
