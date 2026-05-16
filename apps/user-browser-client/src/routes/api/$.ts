ab11af2a70f31afc940c6e8359690c3d8311a004
Ryan Luong <lumintriet@gmail.com>
2026-05-16 10:06:45 +0700

    feat(user-browser-client/chat): add sse/chat route.

--- apps/user-browser-client/src/routes/api/$.ts
+++ apps/user-browser-client/src/routes/api/$.ts
@@ -1,7 +1,31 @@
+import type { MessageDTO } from "@repo/db/dto/message";
+
 import { createFileRoute } from "@tanstack/react-router";
+import { streamSSE } from "hono/streaming";
 import { Hono } from "hono";
 
-const api = new Hono().basePath("/api");
+import { chatEventEmitter } from "@/features/chat/event-emitter";
+
+const api = new Hono().basePath("/api").get("/sse/chat", (c) => {
+  return streamSSE(c, async (stream) => {
+    function newMessageEventListenrer(newMessage: MessageDTO) {
+      const sseMessage = {
+        event: "new-message",
+        id: newMessage.id,
+        data: JSON.stringify(newMessage),
+      };
+      void stream.writeSSE(sseMessage);
+    }
+
+    chatEventEmitter.on("new-message", newMessageEventListenrer);
+
+    // Keep alive
+    while (!stream.aborted) {
+      await stream.sleep(15_000);
+      await stream.writeSSE({ event: "ping", data: "" });
+    }
+  });
+});
 
 export const Route = createFileRoute("/api/$")({
   server: {
