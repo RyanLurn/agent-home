00f7e6bb63c268472399377f117d9fbcd7e8bcb1
Ryan Luong <lumintriet@gmail.com>
2026-05-18 10:20:16 +0700

    chore(db/migration): generate schema and apply migration.

---
+++ packages/db/migrations/20260518031945_majestic_baron_zemo/migration.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `messages` DROP COLUMN `status`;--> statement-breakpoint
+ALTER TABLE `messages` DROP COLUMN `sent_at`;--> statement-breakpoint
+ALTER TABLE `messages` DROP COLUMN `shown_at`;
