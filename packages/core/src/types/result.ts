dcd6e8c28385f79f2f823ce2cd94907c8bb1f46a
Ryan Luong <lumintriet@gmail.com>
2026-05-18 09:16:07 +0700

    feat(core/types): rewrite Result types.

--- packages/core/src/types/result.ts
+++ packages/core/src/types/result.ts
@@ -1,10 +1,42 @@
-export type Result<TData, TError extends Error> =
-  | Failure<TError>
-  | Success<TData>;
-
-export type Failure<TError extends Error> = {
-  success: false;
-  error: TError;
+export type Context<
+  TArgs extends Array<unknown>,
+  TFunctionName extends string,
+> = {
+  args: TArgs;
+  functionName: TFunctionName;
+  performance: {
+    startTime: number;
+    endTime: number;
+    duration: number;
+  };
+  [key: string]: unknown;
 };
 
-export type Success<TData> = { success: true; data: TData };
+export type Failure<
+  TError extends Error,
+  TArgs extends Array<unknown>,
+  TFunctionName extends string,
+> = {
+  success: false;
+  error: TError;
+  context: Context<TArgs, TFunctionName>;
+};
+
+export type Success<
+  TValue,
+  TArgs extends Array<unknown>,
+  TFunctionName extends string,
+> = {
+  success: true;
+  value: TValue;
+  context: Context<TArgs, TFunctionName>;
+};
+
+export type Result<
+  TValue,
+  TError extends Error,
+  TArgs extends Array<unknown>,
+  TFunctionName extends string,
+> =
+  | Success<TValue, TArgs, TFunctionName>
+  | Failure<TError, TArgs, TFunctionName>;
