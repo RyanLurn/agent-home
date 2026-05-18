d55eec79a1f1bedd4c45b1792fb2a99a5810317b
Ryan Luong <lumintriet@gmail.com>
2026-05-18 07:58:46 +0700

    feat(db/schema): add status and its timestamps to messages table.

--- packages/db/src/schema/tables/message.ts
+++ packages/db/src/schema/tables/message.ts
@@ -1,12 +1,16 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
 
+import { MESSAGE_STATUSES, MESSAGE_SENDERS } from "@/schema/constants";
 import { timestampsWithDelete } from "@/schema/helpers/timestamps";
-import { MESSAGE_SENDERS } from "@/schema/constants";
 import { id } from "@/schema/helpers/id";
 
 export const messageTable = sqliteTable("messages", {
   id,
   sender: text("sender", { enum: MESSAGE_SENDERS }).notNull(),
-  content: text().notNull(),
+  content: text("content").notNull(),
+  status: text("status", { enum: MESSAGE_STATUSES }).notNull().default("sent"),
+  sentAt: integer("sent_at", { mode: "timestamp_ms" }).notNull(),
+  shownAt: integer("shown_at", { mode: "timestamp_ms" }),
+  readAt: integer("read_at", { mode: "timestamp_ms" }),
   ...timestampsWithDelete,
 });
