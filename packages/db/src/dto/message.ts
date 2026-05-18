3b03340b7e4690e23ef9458bb66812ea18428e18
Ryan Luong <lumintriet@gmail.com>
2026-05-18 11:16:53 +0700

    feat(db/dto): rewrite MessageDTOSchema to extend SelectedMessageSchema.

--- packages/db/src/dto/message.ts
+++ packages/db/src/dto/message.ts
@@ -2,17 +2,14 @@ import { z } from "zod";
 
 import type { SelectedMessage } from "@/types";
 
-import { MESSAGE_SENDERS } from "@/schema/constants";
+import { SelectedMessageSchema } from "@/schema/zod/message";
 
 export function createMessageDTO(selectedMessage: SelectedMessage) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { deletedAt, sentAt, shownAt, readAt, createdAt, updatedAt, ...rest } =
-    selectedMessage;
+  const { deletedAt, readAt, createdAt, updatedAt, ...rest } = selectedMessage;
 
   return {
     ...rest,
-    sentAt: sentAt.toISOString(),
-    shownAt: shownAt === null ? null : shownAt.toISOString(),
     readAt: readAt === null ? null : readAt.toISOString(),
     createdAt: createdAt.toISOString(),
     updatedAt: updatedAt.toISOString(),
@@ -20,9 +17,8 @@ export function createMessageDTO(selectedMessage: SelectedMessage) {
 }
 
 export const MessageDTOSchema = z.strictObject({
-  id: z.uuidv7(),
-  sender: z.enum(MESSAGE_SENDERS),
-  content: z.string(),
+  ...SelectedMessageSchema.omit({ deletedAt: true }).shape,
+  readAt: z.iso.datetime().nullable(),
   createdAt: z.iso.datetime(),
   updatedAt: z.iso.datetime(),
 });
