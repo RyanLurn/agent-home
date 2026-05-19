000b432bd6575823154fcc66010ca678d0c34c93
Ryan Luong <lumintriet@gmail.com>
2026-05-19 10:44:25 +0700

    feat(user-web): define the default error component.

--- apps/user-web/src/router.ts
+++ apps/user-web/src/router.ts
@@ -1,11 +1,13 @@
 import { DefaultNotFound } from "@repo/ui/components/pages/default-not-found";
 import { createRouter } from "@tanstack/react-router";
 
+import { DefaultErrorComponent } from "@/components/default-error";
 import { routeTree } from "@/routeTree.gen";
 
 export function getRouter() {
   const router = createRouter({
     defaultNotFoundComponent: DefaultNotFound,
+    defaultErrorComponent: DefaultErrorComponent,
     scrollRestoration: true,
     routeTree,
   });
