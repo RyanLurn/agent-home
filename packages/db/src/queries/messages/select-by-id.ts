4f0afaf0c12483d30493c3bd33e4fc773e9a75df
Ryan Luong <lumintriet@gmail.com>
2026-05-19 09:21:19 +0700

    feat(db/queries): rewrite selectMessageById function to use the new Result type.

--- packages/db/src/queries/messages/select-by-id.ts
+++ packages/db/src/queries/messages/select-by-id.ts
@@ -1,4 +1,5 @@
-import type { Result } from "@repo/core/types/result";
+import type { Context, Result } from "@repo/core/types/result";
+import type { StrictOmit } from "@repo/core/types/strict-omit";
 
 import {
   createFallbackError,
@@ -6,10 +7,12 @@ import {
 } from "@repo/core/error/create-fallback";
 import { NotFoundError } from "@repo/core/error/classes/not-found";
 import { isNull, and, eq } from "drizzle-orm";
+import { SQLiteError } from "bun:sqlite";
 
 import type { SelectedMessage } from "@/types";
 
 import { createMessageDTO, type MessageDTO } from "@/dto/message";
+import { UnexpectedDatabaseError } from "@/errors/unexpected";
 import { messageTable } from "@/schema/tables/message";
 import { db } from "@/index";
 
@@ -17,11 +20,18 @@ export async function selectMessageById({
   id,
 }: {
   id: SelectedMessage["id"];
-}): Promise<Result<MessageDTO, NotFoundError<"message"> | FallBackError>> {
-  const startContext = {
-    inputs: { id },
-    processName: "selectMessageById",
+}): Promise<
+  Result<
+    MessageDTO,
+    NotFoundError<"messages"> | UnexpectedDatabaseError | FallBackError
+  >
+> {
+  const sharedContext: StrictOmit<Context, "performance"> = {
+    functionName: "selectMessageById" as const,
+    args: { id },
+    metadata: {},
   };
+  const startTime = performance.now();
 
   try {
     const [selectedMessage] = await db
@@ -30,26 +40,57 @@ export async function selectMessageById({
       .where(and(eq(messageTable.id, id), isNull(messageTable.deletedAt)));
 
     if (selectedMessage) {
+      const returnedMessage = createMessageDTO(selectedMessage);
+
+      const endTime = performance.now();
       return {
         success: true,
-        data: createMessageDTO(selectedMessage),
+        value: returnedMessage,
+        context: {
+          ...sharedContext,
+          performance: {
+            startTime,
+            endTime,
+            duration: endTime - startTime,
+          },
+        },
       };
+    }
+
+    const endTime = performance.now();
+    return {
+      success: false,
+      error: new NotFoundError<"messages">({ resource: "messages" }),
+      context: {
+        ...sharedContext,
+        performance: {
+          startTime,
+          endTime,
+          duration: endTime - startTime,
+        },
+      },
+    };
+  } catch (thrownError) {
+    const endTime = performance.now();
+
+    let error: UnexpectedDatabaseError | FallBackError;
+    if (thrownError instanceof SQLiteError) {
+      error = new UnexpectedDatabaseError({ cause: thrownError });
+    } else {
+      error = createFallbackError({ cause: thrownError });
     }
 
     return {
       success: false,
-      error: new NotFoundError<"message">({
-        context: { ...startContext, resource: "message" },
-      }),
-    };
-  } catch (error) {
-    return {
-      success: false,
-      error: createFallbackError({
-        message: "Failed to select message due to an unexpected error",
-        context: startContext,
-        cause: error,
-      }),
+      error,
+      context: {
+        ...sharedContext,
+        performance: {
+          startTime,
+          endTime,
+          duration: endTime - startTime,
+        },
+      },
     };
   }
 }
