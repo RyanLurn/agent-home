999fa9f37c3dadea1b181c5f41fc146996c73fcd
Ryan Luong <lumintriet@gmail.com>
2026-05-19 11:08:48 +0700

    feat(user-web): make logging more robust.

--- apps/user-web/src/features/chat/server-functions/get-all-messages.ts
+++ apps/user-web/src/features/chat/server-functions/get-all-messages.ts
@@ -1,19 +1,64 @@
+import type { Failure, Success } from "@repo/core/types/result";
+import type { MessageDTO } from "@repo/db/dto/message";
+
 import { selectAllMessages } from "@repo/db/queries/messages/select-all";
+import { getRequest } from "@tanstack/react-start/server";
 import { createServerFn } from "@tanstack/react-start";
 
 export const getAllMessages = createServerFn().handler(async () => {
-  const result = await selectAllMessages();
+  const startTime = performance.now();
 
-  if (result.success) {
-    console.log(result);
-    return result.value;
+  const request = getRequest();
+
+  const selectAllMessagesResult = await selectAllMessages();
+
+  if (selectAllMessagesResult.success) {
+    const allMessages = selectAllMessagesResult.value;
+
+    const endTime = performance.now();
+    const getAllMessagesResult: Success<MessageDTO[]> = {
+      success: true,
+      value: allMessages,
+      context: {
+        functionName: "getAllMessages" as const,
+        args: {},
+        metadata: {
+          request,
+          selectAllMessagesResult,
+        },
+        performance: {
+          startTime,
+          endTime,
+          duration: endTime - startTime,
+        },
+      },
+    };
+    console.log(getAllMessagesResult);
+
+    return allMessages;
   }
 
-  console.error(result);
-  throw new Error(
-    JSON.stringify({
-      code: result.error.code,
-      message: "Failed to load messages.",
-    })
-  );
+  const error = new Error("Failed to load messages.");
+
+  const endTime = performance.now();
+  const getAllMessagesResult: Failure<Error> = {
+    success: false,
+    error,
+    context: {
+      functionName: "getAllMessages" as const,
+      args: {},
+      metadata: {
+        request,
+        selectAllMessagesResult,
+      },
+      performance: {
+        startTime,
+        endTime,
+        duration: endTime - startTime,
+      },
+    },
+  };
+  console.error(getAllMessagesResult);
+
+  throw error;
 });
