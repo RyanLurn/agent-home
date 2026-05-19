25b540c607426755d4ac0d8f31fd232f9b1c5879
Ryan Luong <lumintriet@gmail.com>
2026-05-19 10:31:35 +0700

    feat(user-web/chat) rewrite the getAllMessages server function to use the new Result object.

--- apps/user-web/src/features/chat/server-functions/get-all-messages.ts
+++ apps/user-web/src/features/chat/server-functions/get-all-messages.ts
@@ -2,6 +2,18 @@ import { selectAllMessages } from "@repo/db/queries/messages/select-all";
 import { createServerFn } from "@tanstack/react-start";
 
 export const getAllMessages = createServerFn().handler(async () => {
-  const messages = await selectAllMessages();
-  return messages;
+  const result = await selectAllMessages();
+
+  if (result.success) {
+    console.log(result);
+    return result.value;
+  }
+
+  console.error(result);
+  throw new Error(
+    JSON.stringify({
+      code: result.error.code,
+      message: "Failed to load messages.",
+    })
+  );
 });
